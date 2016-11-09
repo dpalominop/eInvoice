b9f20b5b6432ca91fe38e4a962161e9a31bbbe54
Daniel Palomino <dapalominop@gmail.com>
2016-11-08 22:05:00 -0500

    Nuevas Urls del WebService de la SUNAT.

--- webservice/conf.py
+++ webservice/conf.py
@@ -1,11 +1,14 @@
-
+### Antiguo WebService
 ### SUNAT_WS = 'https://www.sunat.gob.pe:443/ol-ti-itcpgem-beta/billService?wsdl'
 
-### Factura. Notas vinculadas. Servicios Públicos. Resumen Diario. Comunicación de Baja. Lotes de Facturas.
-SUNAT_WS = 'https://e-factura.sunat.gob.pe/ol-ti-itcpfegem/billService?wsdl'
+### Factura. Notas vinculadas. Servicios Publicos. Resumen Diario. Comunicacion de Baja. Lotes de Facturas.
+### SUNAT_WS = 'https://e-factura.sunat.gob.pe/ol-ti-itcpfegem/billService?wsdl'
 
-### Retención y Percepción Resumen de Reversión de comprobantes de percepción o retención
-### SUNAT_WS = https://www.sunat.gob.pe/ol-ti-itemision-otroscpe-gem/billService?wsdl
+### Retencion y Percepcion Resumen de Reversion de comprobantes de percepcion o retencion
+### SUNAT_WS = 'https://www.sunat.gob.pe/ol-ti-itemision-otroscpe-gem/billService?wsdl'
 
-###Guía de Remisión Remitente Electrónica
-### SUNAT_WS = https://e-guiaremision.sunat.gob.pe/ol-ti-itemision-guia-gem-beta/billService?wsdl
+###Guia de Remision Remitente Electronica
+SUNAT_WS = 'https://e-guiaremision.sunat.gob.pe/ol-ti-itemision-guia-gem-beta/billService?wsdl'
+
+### Proceso de Homologacion
+### SUNAT_WS = 'https://www.sunat.gob.pe/ol-ti-itcpgem-sqa/billService'
