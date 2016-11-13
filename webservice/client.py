cf5405d4fc5bcb8f9c8fa174ddd0c15834d07ca9
Daniel Palomino <dapalominop@gmail.com>
2016-11-13 10:14:32 -0500

    Inclusión de comentarios.

--- webservice/client.py
+++ webservice/client.py
@@ -30,49 +30,59 @@ class Client(object):
             print(ex)
             return None
     
-    #Recibe un archivo zip con un unico documento XML de comprobante.
-    #Devuelve un archivo zip con un documento XML que es la constancia de aceptacion o rechazo.
     def sendBill(self, filename, content_file):
+        """
+        Recibe un archivo zip con un unico documento XML de comprobante.
+        Devuelve un archivo zip con un documento XML que es la constancia de aceptacion o rechazo.
+        """
         params = {
             'fileName': filename,
             'contentFile': content_file
         }
         return self._call_service('sendBill', params)
 
-    #Recibe un archivo zip con un unico documento XML de resumenes
-    #(resumen de boletas, comunicacion de baja, reversiones de comprobantes de percepció y recepcion).
-    #Devuelve un ticket (Str) con el que usando el metodo getStatus se obtiene el archivo zip 
-    #conteniendo el documento XML que es la constancia de aceptacion o rechazo.
     def sendSummary(self, filename, content_file):
+        """
+        Recibe un archivo zip con un unico documento XML de resumenes
+        (resumen de boletas, comunicacion de baja, reversiones de comprobantes de percepció y recepcion).
+        Devuelve un ticket (Str) con el que usando el metodo getStatus se obtiene el archivo zip 
+        conteniendo el documento XML que es la constancia de aceptacion o rechazo.
+        """
         params = {
             'fileName': filename,
             'contentFile': content_file
         }
         return self._call_service('sendSummary', params)
 
-    #Recibe un archivo zip con un unico documento XML de resumenes
-    #(facturas, boletas de venta, notas de credito y debito).
-    #Devuelve un ticket (Str) con el que usando el metodo getStatus se obtiene el archivo zip 
-    #conteniendo varios documentos XML que es la constancia de aceptacion o rechazo por documento enviado y un archivo resumen.
     def sendPack(self, filename, content_file):
+        """
+        Recibe un archivo zip con un unico documento XML de resumenes
+        (facturas, boletas de venta, notas de credito y debito).
+        Devuelve un ticket (Str) con el que usando el metodo getStatus se obtiene el archivo zip 
+        conteniendo varios documentos XML que es la constancia de aceptacion o rechazo por documento enviado y un archivo resumen.
+        """
         params = {
             'fileName': filename,
             'contentFile': content_file
         }
         return self._call_service('sendPack', params)
     
-    #Recibe el ticket (Str)
-    #Devuelve un objeto que indica el estado del proceso y en caso de haber terminado,
-    # devuelve adjunta la constancia de aceptacion o rechazo y el reporte de envio (para el caso de lotes)
     def getStatus(self, ticket):
+        """
+        Recibe el ticket (Str)
+        Devuelve un objeto que indica el estado del proceso y en caso de haber terminado,
+        devuelve adjunta la constancia de aceptacion o rechazo y el reporte de envio (para el caso de lotes)
+        """
         params = {
             'ticket': ticket
         }
         return self._call_service('getStatus', params)
 
-    #Recibe como parametro los datos de comprobante de pago (ruc del emisor, tipo de comprobante, serie y numero de comprobante).
-    #Devuelve un archivo zip que contiene el documento XML de la constancia de recepcion.
     def getStatusCdr(self, rucComprobante):
+        """
+        Recibe como parametro los datos de comprobante de pago (ruc del emisor, tipo de comprobante, serie y numero de comprobante).
+        Devuelve un archivo zip que contiene el documento XML de la constancia de recepcion.
+        """
         params = {
             'rucComprobante': rucComprobante
         }
